5ab8253b5e6b8b9d6fc8f77a8c7228935b196d3c
root <root@portal.hawagym.id>
2020-12-09 08:27:25 +0700

    init

--- rules.sh
+++ rules.sh
@@ -6,4 +6,6 @@ read -p 'Insert Many Hit : ' HIT
 echo "sudo iptables -$METHOD INPUT -i eth0 -p tcp --dport $PORT -m state --state NEW -m recent --set --name SSH"
 echo "sudo iptables -$METHOD INPUT -i eth0 -p tcp --dport $PORT -m state --state NEW -m recent --update --seconds $TIME --hitcount $HIT --rttl --name SSH -j DROP"
 
+echo '====================='
+echo '[*] Dont Forget For Save Rules With iptables-persistent'
 
